49255d45d7156da27c1f6973401bcca70e9b4550
renè <rene.menditto@libero.it>
2025-04-06 11:47:01 +0200

    ** gestione errori **

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -4,13 +4,22 @@ import { Post } from "@prisma/client";
 
 export async function GET() {
   let posts: Post[] = [];
-  posts = await prisma.post.findMany({
-    include: { author: true },
-  });
+  try {
+    posts = await prisma.post.findMany({
+      include: { author: true },
+    });
+  } catch (error) {
+    console.error("Errore durante il recupero dei post:", error);
+    return NextResponse.json(
+      { error: "Errore durante il recupero dei post" },
+      { status: 500 }
+    );
+  }
   return NextResponse.json(posts);
 }
 
 export async function POST(req: NextRequest) {
+  let post: Post | null = null;
   const { title, content, authorId, published } = (await req.json()) as Post;
   if (!title || !content) {
     return NextResponse.json(
@@ -18,8 +27,21 @@ export async function POST(req: NextRequest) {
       { status: 400 }
     );
   }
-  const post = await prisma.post.create({
-    data: { title, content, author: { connect: { id: authorId } }, published },
-  });
+  try {
+    post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        author: { connect: { id: authorId } },
+        published,
+      },
+    });
+  } catch (error) {
+    console.error("Errore durante la creazione del post:", error);
+    return NextResponse.json(
+      { error: "Errore durante la creazione del post" },
+      { status: 500 }
+    );
+  }
   return NextResponse.json(post);
 }
